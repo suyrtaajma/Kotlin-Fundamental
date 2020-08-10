fb063ab4b72d4980b3cf35e8a64bd6f7e6af8741
suyrtaajma <suryatmaja@protonmail.com>
2020-08-10 11:14:32 +0700

    Create Boolean.kt

---
+++ src/main/kotlin/Boolean.kt
@@ -0,0 +1,29 @@
+fun main() {
+    var now = 60
+    val nilai: String
+    val nilaiC = now < 70
+    val nilaiE = now >= 50
+    nilai = if (now > 90 && now <= now) {
+        "A"
+    } else if (now > 80 || now > 80) {
+        "B"
+    } else if (!nilaiC) {
+        "C"
+    } else if (now > 60 || now > 60) {
+        "D"
+    } else if (!nilaiE){
+        "E"
+    } else{
+        "Error: no data found"
+    }
+    println(nilai)
+}
+
+/*
+>90 = A
+>80 = B
+>70 = C
+>60 = D
+>50 = ??
+<=50 = E
+*/
